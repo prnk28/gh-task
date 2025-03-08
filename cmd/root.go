72ab1af1207ceb9fff353dac7085734489cbbcce
Prad N <prad@didao.xyz>
2025-03-07 19:12:34 -0500

    refactor: remove viper configuration

--- cmd/root.go
+++ cmd/root.go
@@ -4,13 +4,11 @@ Copyright © 2025 Prad N i@prad.nu
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/prnk28/gh-task/internal/ctx"
 	"github.com/prnk28/gh-task/internal/ghc"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -56,40 +54,40 @@ func Execute() {
 	}
 }
 
-func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gh-task.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".gh-task" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".gh-task")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
-}
+// func init() {
+// 	cobra.OnInitialize(initConfig)
+//
+// 	// Here you will define your flags and configuration settings.
+// 	// Cobra supports persistent flags, which, if defined here,
+// 	// will be global for your application.
+//
+// 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gh-task.yaml)")
+//
+// 	// Cobra also supports local flags, which will only run
+// 	// when this action is called directly.
+// 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+// }
+//
+// // initConfig reads in config file and ENV variables if set.
+// func initConfig() {
+// 	if cfgFile != "" {
+// 		// Use config file from the flag.
+// 		viper.SetConfigFile(cfgFile)
+// 	} else {
+// 		// Find home directory.
+// 		home, err := os.UserHomeDir()
+// 		cobra.CheckErr(err)
+//
+// 		// Search config in home directory with name ".gh-task" (without extension).
+// 		viper.AddConfigPath(home)
+// 		viper.SetConfigType("yaml")
+// 		viper.SetConfigName(".gh-task")
+// 	}
+//
+// 	viper.AutomaticEnv() // read in environment variables that match
+//
+// 	// If a config file is found, read it in.
+// 	if err := viper.ReadInConfig(); err == nil {
+// 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+// 	}
+// }
